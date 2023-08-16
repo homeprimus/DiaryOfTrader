3214359ed2b3e718f06b4b681f25c10aa204d042
Nikita Posokhov <kenismiz@gmail.com>
2023-08-16 21:53:41 +0300

    ...

---
+++ DiaryOfTrader.Web/Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using DiaryOfTrader.Web;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
+await builder.Build().RunAsync();
